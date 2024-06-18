fc18a0593af25855d8e8782cf24d631f4d1bf325
Kauã Severgnini <kauasevergnini9@gmail.com>
2024-06-18 18:27:51 -0300

    Aula sobre criação e movimento de entidades

--- MontainSchouter1/Code/Const.py
+++ MontainSchouter1/Code/Const.py
@@ -1,2 +1,25 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 128)
+COLOR_BLACK_TR = (0, 0, 0, 150)  # Cor do fundo: preto com 150 de opacidade (transparência)
+
+# M
+MENU_OPTION = ('NEM GAME 1P',
+               'NEM GAME 2P - COOPERATIVE',
+               'NEM GAME 2P - COMPETITIVE',
+               'EXIT')
+
+# W
 WIN_WIDTH = 576
 WIN_HEIGTH = 324
+
+# E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 1,
+                'Level1Bg2': 2,
+                'Level1Bg3': 3,
+                'Level1Bg4': 4,
+                'Level1Bg5': 5,
+                'Level1Bg6': 6,
+                }
--- MontainSchouter1/Code/Game.py
+++ MontainSchouter1/Code/Game.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame as pygame
-from pygame import Rect, Surface
-from pygame.font import Font
+import sys
 
-from Code.Const import WIN_WIDTH, WIN_HEIGTH
+import pygame as pygame
+
+from Code.Const import WIN_WIDTH, WIN_HEIGTH, MENU_OPTION
+from Code.Level import Level
 from Code.Menu import Menu
 
 
@@ -16,6 +17,12 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
 
